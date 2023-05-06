b0bdfe15092dff5d7713389cec99c2f69804bbba
davidrodriguezz <davidr13@ucm.es>
2023-05-06 14:22:30 +0200

    Añadidos comentarios y quitados los outputs de los require

--- QuadraticVoting.sol
+++ QuadraticVoting.sol
@@ -6,17 +6,17 @@ import "contracts/Proposal.sol";
 
 contract QuadraticVoting {
 
-    address owner;
-    uint256 budget;
-    uint256 price;
-    uint256 lastId;
-    uint256 num_participants;
-    uint256 num_pending_proposals;
-    bool is_open;
+    address owner;      //Owner del contrato
+    uint256 budget;     //Presupuesto
+    uint256 price;      //Precio de los tokens
+    uint256 lastId;     //Guardamos el id de cada una de las propuestas a partir de 1
+    uint256 num_participants;   //Numero de participantes
+    uint256 num_pending_proposals;  //Numero de propuestas pendientes
+    bool is_open;   //Votacion abierta
 
-    TokenManager tm;
+    TokenManager tm;    //Tokens
 
-    struct proposal {
+    struct proposal {   //Guarda informacion de cada propuesta y de sus votantes
         string title;
         string description;
         uint256 proposal_budget;
@@ -28,16 +28,16 @@ contract QuadraticVoting {
         address[] voters;
     }
 
-    struct Paticipant {
+    struct Paticipant { //Informacion de los participantes sobre sus tokens y lo que han votado
         uint248 tokens;
         uint8 exist;
         mapping (uint256 => uint256) votes;
     }
 
     mapping (address => uint256) proposals_aux;
-    mapping (address => Paticipant) participants;
-    mapping (uint256 => proposal) proposals;
-    uint256 [] array_proposals;
+    mapping (address => Paticipant) participants;   //Mapping de direccion y participante
+    mapping (uint256 => proposal) proposals;        //Mapping de id con la propuesta
+    uint256 [] array_proposals; //Array de propuestas
 
     constructor (uint256 _price, uint256 _max_tokens) {
         owner = msg.sender;
@@ -48,46 +48,51 @@ contract QuadraticVoting {
         num_pending_proposals = 0;
         tm = new TokenManager(_max_tokens);
     }
-   
+    // Modificador para comprobar si votas/retiras mas de 0 votos
+    modifier enoughVotes (uint256 num_votes){
+        require(num_votes > 0);
+        _;
+    }
+    // Modificador para comprobar si es el duenho del contrato
     modifier isOwnerContract{
-        require (owner==msg.sender, "You are not the owner");
+        require (owner==msg.sender);
         _;
     }
-
+    // Modificador para comprobar si la votacion esta abierta
     modifier isOpen{
-        require (is_open==true, "The voting is not open");
+        require (is_open==true);
         _;
     }
-
+    //Modificador para compronar si una propuesta esta aprobada
     modifier isApproved(uint256 id){
-        require (!proposals[id].approved, "The proposal has been approved");
+        require (!proposals[id].approved);
         _;
     }
-
+    //Modificador para comprobar si es el creador de la propuesta
     modifier proposalCreator(uint256 id){
-        require (proposals[id].creator==msg.sender, "You are not the creater of this proposal");
+        require (proposals[id].creator==msg.sender);
         _;
     }
-
+    //Modificador para comprobar si existe un participante
     modifier registeredParticipant {
-        require (participants[msg.sender].exist == 1, "You are not register as participant");
+        require (participants[msg.sender].exist == 1);
         _;
     }
 
 
     function openVoting() external payable isOwnerContract {
-        require(msg.value > 0, "You must open the voting with any budget");
+        require(msg.value > 0); //Necesitas abrir el proceso de votacion con un presupuesto
         budget = msg.value;
         is_open = true;
     }
 
     function addParticipant() external payable {
-        require(participants[msg.sender].exist == 0, "Participant already exist");
+        require(participants[msg.sender].exist == 0);  //El participante existe
         uint256 tokens = msg.value/price;
-        require(tokens > 0, "You must deposit at least 1 token");
-        tm.mint(msg.sender, tokens);
-        participants[msg.sender].tokens = uint248(tokens);
-        participants[msg.sender].exist = 1;
+        require(tokens > 0);   //Necesitas depositar al menos 1 token
+        tm.mint(msg.sender, tokens);            //Se minan los tokens correspondientes
+        participants[msg.sender].tokens = uint248(tokens);  //Para que ocupe menos en memoria, en un mismo bloque guardamos el número de 
+        participants[msg.sender].exist = 1;     //tokens y si existe o no como participante
         ++num_participants;
     }
 
@@ -97,7 +102,7 @@ contract QuadraticVoting {
     }
 
     function addProposal(string memory title, string memory description, uint256 proposal_budget, address a) external isOpen returns (uint){
-        require (a != address(0), "This proposal don't exists");
+        require (a != address(0));    //La propuesta no existe
         proposals[lastId].title = title;
         proposals[lastId].description = description;
         proposals[lastId].proposal_budget = proposal_budget;
@@ -126,13 +131,13 @@ contract QuadraticVoting {
 
     function buyTokens() external payable registeredParticipant {
         uint256 tokens = msg.value/price;
-        tm.mint(msg.sender, tokens);
+        tm.mint(msg.sender, tokens); //Minamos tokens y se lo anhadimos al participante
         participants[msg.sender].tokens += uint248(tokens);
     }
 
     function sellTokens() external registeredParticipant {
         uint256 tokens = participants[msg.sender].tokens;
-        tm.transferFrom(msg.sender, address(this), tokens);
+        tm.transferFrom(msg.sender, address(this), tokens); //Le devolvemos su dinero y quemamos los tokens
         tm.burn(tokens);
         participants[msg.sender].tokens = 0;
         payable(msg.sender).transfer(tokens*price);
@@ -165,7 +170,7 @@ contract QuadraticVoting {
         }
         uint256[] memory result = new uint256[](index);
         for (uint256 i = 0; i < index; ++i) result[i] = approved_proposals[i];
-        assembly{mstore(result,index)}
+        assembly{mstore(result,index)}  //Guardamos el array de aprobadas
         return result;
     }
  
@@ -180,19 +185,17 @@ contract QuadraticVoting {
         }
         uint256[] memory result = new uint256[](index);
         for (uint256 i = 0; i < index; ++i) result[i] = signaling_proposals[i];
-        assembly{mstore(result,index)}
+        assembly{mstore(result,index)}  //Guardamos el array de signal
         return result;
     }
 
     function getProposalInfo(uint256 id) external view isOpen returns(string memory, string memory, uint, address) {
-        return (proposals[id].title, proposals[id].description, proposals[id].proposal_budget, proposals[id].proposal_address);
+        return (proposals[id].title, proposals[id].description, proposals[id].proposal_budget, proposals[id].proposal_address); //Damos solo la informacion basica
     }
 
-    function stake(uint256 id, uint256 num_votes) external {
-        require(num_votes > 0, "The number of votes must be higher than zero");
-        require(!proposals[id].approved, "The proposal has been approved");
+    function stake(uint256 id, uint256 num_votes) external enoughVotes(num_votes) isApproved(id){
         uint256 tokens = (participants[msg.sender].votes[id] + num_votes)**2 - (participants[msg.sender].votes[id])**2;
-        require (participants[msg.sender].tokens >= tokens, "You don't have enough token to vote");
+        require (participants[msg.sender].tokens >= tokens);    //No tiene tokens para votar
         if(participants[msg.sender].votes[id] == 0) proposals[id].voters.push(msg.sender);
         participants[msg.sender].votes[id] += num_votes;
         participants[msg.sender].tokens -= uint248(tokens);
@@ -202,10 +205,8 @@ contract QuadraticVoting {
         _checkAndExecuteProposal(id);
     }
 
-    function withdrawFromProposal(uint256 id, uint256 num_votes) external{
-        require(num_votes > 0, "The number of votes must be higher than zero");
-        require(!proposals[id].approved, "The proposal has been approved");
-        require(participants[msg.sender].votes[id] >= num_votes, "You don't have enough votes to withdraw");
+    function withdrawFromProposal(uint256 id, uint256 num_votes) external enoughVotes(num_votes) isApproved(id){
+        require(participants[msg.sender].votes[id] >= num_votes);   //No tiene los votos para retirarlos
         uint256 tokens = (participants[msg.sender].votes[id])**2 - (participants[msg.sender].votes[id] - num_votes)**2;
         participants[msg.sender].votes[id] -= num_votes;
         participants[msg.sender].tokens += uint248(tokens);
@@ -220,7 +221,7 @@ contract QuadraticVoting {
             proposals[id].approved = true;
         }
         uint256 threshold;
-        if(budget != 0) threshold = (2 * num_participants + ((proposals[id].proposal_budget * num_participants) / (budget * price))) + num_pending_proposals * 10;
+        if(budget != 0) threshold = (2 * num_participants + ((proposals[id].proposal_budget * num_participants) / (budget * price))) + num_pending_proposals * 10;  //Calculo de umbral
         else threshold = num_pending_proposals;
         if(proposals[id].proposal_budget <= budget + (proposals[id].num_tokens * price) && (proposals[id].votes*10) > threshold) {
             Proposal(proposals[id].proposal_address).executeProposal{value: proposals[id].proposal_budget, gas:10000}(id, proposals[id].num_tokens, proposals[id].num_tokens);
@@ -244,12 +245,12 @@ contract QuadraticVoting {
                     tm.transfer(proposals[id].voters[i], tokens);
                 }
             }
-            delete proposals[id];
+            delete proposals[id];       //Eliminamos cada una de las propuestas al cerrar la votacion para poder realizar otros sistemas de votacion de manera posterior
         }
         payable(owner).transfer(budget);
         lastId = 1;
-        num_participants = 0;
-        delete array_proposals;
-        is_open = false;
+        num_participants = 0;   //Eliminamos a los participantes
+        delete array_proposals; //Borramos el array de propuestas por el mismo motivo
+        is_open = false;    //Cerramos la votacion
     }
 }
